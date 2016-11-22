d861d59786bce1cf24129ebc05245136a2b4c1c7
tcash21 <tanyacash@gmail.com>
2016-11-22 14:00:49 -0500

    removed absolute directory path

--- SenateCaller/senator_numbers.R
+++ SenateCaller/senator_numbers.R
@@ -13,4 +13,4 @@ phones_no_punct <- phones_no_punct[-which(is.na(phones_no_punct))]
 
 final_phones <- paste0("+1", phones_no_punct)
 
-write.csv(file="~/Documents/Twilio/senator_phones.csv", final_phones, row.names = FALSE)
+write.csv(file="senator_phones.csv", final_phones, row.names = FALSE)
